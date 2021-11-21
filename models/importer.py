dd684227659d4b8167e01652dcb2ecb755df00f7
Paimon <n.h.shounen@gmail.com>
2021-11-21 20:46:02 +0700

    prepare function for output per type file

--- models/importer.py
+++ models/importer.py
@@ -2,3 +2,12 @@ from odoo import models, fields
 
 class Importer(models.TransientModel):
     _name = 'paimon.importer'
+
+    def output_xls():
+        pass
+
+    def output_csv():
+        pass
+
+    def output_ods():
+        pass
