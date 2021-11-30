a584d05867a4006b5d453809714b1efcf67e469f
Paimon <n.h.shounen@gmail.com>
2021-11-30 19:41:14 +0700

    add example format excel

--- models/example.py
+++ models/example.py
@@ -3,3 +3,16 @@ from odoo import models
 class Example(models.TransientModel):
     _name = 'paimon.example'
     _inherit = 'paimon.importer'
+
+    def output_xls(self, rows):
+        """
+        # @INPUT EXCEL
+        =============================================================
+        | name              | description           | date          |
+        -------------------------------------------------------------
+        | product 01        | product 01 desc       | 2021-11-31    |
+        -------------------------------------------------------------
+        | product 02        | product 02 desc       | 2021-11-15    |
+        ------------------------------------------------------------
+        """
+        pass
