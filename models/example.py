f3289ce299af40e241f27e4f9a1520fccfc0ba3a
Paimon <n.h.shounen@gmail.com>
2021-11-30 19:45:38 +0700

    add example value for each iterate

--- models/example.py
+++ models/example.py
@@ -15,4 +15,8 @@ class Example(models.TransientModel):
         | product 02        | product 02 desc       | 2021-11-15    |
         ------------------------------------------------------------
         """
-        pass
+        
+        for row in rows:
+            row[0]  # for each iterate will print `name`, `product 01`, `product 02`
+            row[1]  # for each iterate will print `description`, `product 01 desc`, `product 02 desc`
+            row[2]  # for each iterate will print `date`, datetime(2021-11-31), datetime(2021-11-15)
